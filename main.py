78e29c1f2808ba809d974e14d54e01dc4db7a057
Konstantinos Bazakos <kbazakos@gmail.com>
2022-12-06 23:02:37 +0200

    Optimised a bit!

--- main.py
+++ main.py
@@ -1,43 +1,13 @@
 from pdf2image import convert_from_path
 import os
-from pdf2image.exceptions import (
-    PDFInfoNotInstalledError,
-    PDFPageCountError,
-    PDFSyntaxError
-)
+
 import tempfile
-
 poppler_path = r"Release-22.12.0-0/poppler-22.12.0/Library/bin"
-# pdf_path = r"pdf_files/2.pdf"
 output_folder = r"output_files"
-#
-# pages = convert_from_path(pdf_path=pdf_path,
-#                           poppler_path=poppler_path,
-#                           grayscale=True,
-#                           size=(1700, 2400),
-#                           dpi=300
-#                           )
-#
-#
-# for index, page in enumerate(pages):
-#     page.save(os.path.join(output_folder, f"{index}.jpg"), "JPEG")
 
-# for subdir, dirs, files in os.walk('pdf_files'):
-#     if files:
-#         for file in files:
-#             pdf_path = f'{subdir}/{file}'
-#             pages = convert_from_path(pdf_path=pdf_path,
-#                                       poppler_path=poppler_path,
-#                                       grayscale=True,
-#                                       size=(1700, 2400),
-#                                       dpi=300
-#                                       )
-#             if not os.path.exists(f'{output_folder}/{file}'):
-#                 os.makedirs(f'{output_folder}/{file}'[:-4])
-#                 for index, page in enumerate(pages):
-#                     page.save(os.path.join(f'{output_folder}/{file}'[:-4], f"{index}.jpg"), "JPEG")
+
 for subdir, dirs, files in os.walk('pdf_files'):
-    print(subdir, dirs, files)
+    # print(subdir, dirs, files)
     if files:
         for file in files:
             pdf_path = f'{subdir}/{file}'
@@ -46,16 +16,17 @@ for subdir, dirs, files in os.walk('pdf_files'):
                                                      poppler_path=poppler_path,
                                                      grayscale=True,
                                                      size=(1700, 2400),
-                                                     dpi=300
+                                                     dpi=300,
+                                                     fmt="jpeg",
+                                                     jpegopt={
+                                                         "quality": 100,
+                                                         "progressive": False,
+                                                         "optimize": False
+                                                     }
                                                      )
                 if not os.path.exists(f'{output_folder}/{file}'):
                     os.makedirs(f'{output_folder}/{file}'[:-4])
                     for index, page in enumerate(images_from_path):
-                        page.save(os.path.join(f'{output_folder}/{file}'[:-4], f"{index}.jpg"), "JPEG")
-#
-# convert_from_path(pdf_path, dpi=200, output_folder=None, first_page=None,
-#                   last_page=None, fmt='ppm', jpegopt=None, thread_count=1,
-#                   userpw=None, use_cropbox=False, strict=False, transparent=False,
-#                   single_file=False, output_file=str(uuid.uuid4()), poppler_path=None,
-#                   grayscale=False, size=None, paths_only=False, use_pdftocairo=False, timeout=600)
+                        page.save(os.path.join(f'{output_folder}/{file}'[:-4], f"{index}.jpg"), "JPEG", dpi=(300, 300))
 
+# https://github.com/Belval/pdf2image
